2a1944b3ff381f88f89142f7cb3ef58f6a047642
Natnial Asegdew <natnial99@gmail.com>
2026-08-26 13:55:50 +0300

    Adding Banner.jsx

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 function Banner() {
     return (
-        <div>
+        <div className={styles.banner}>
+            <div className={styles.contents}>
+                <img className={styles.logoImg} src={bannerLogo} alt="Netflix Logo" />
+                <h1 className={styles.title}>Bridgerton</h1>
 
-        </div>
+                <h1 className={styles.description}>Hey hsdbhs sbshs shbashd sad hdbshad hd sha</h1>
+
+                <div className={styles.buttonContainer}>
+                    <button className={styles.button}>
+                        <Play size={30} />
+                        Play
+                    </button>
+                    <button className={styles.button}>
+                        <Info size={30} />
+                        My List
+                    </button>
+                </div>
+
+
+            </div >
+            <div className={styles.fadeBottom}></div>
+        </div >
     )
 }
 
