f8f3e4fa55108753aed34063b98dacae2f8d05a4
Anan1729 <ananyawub20@gmail.com>
2026-08-27 16:28:41 -0700

    feat(header): implement base layout structural containers and navigation menus

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/image/logo.png"; 
+
+function Header() {
+  return (
+    <header className="header-container">
+      {/* LeftSide: LOGO and Nav-links */}
+      <div className="header-left">
+        <Link to="/">
+          <img className="header-logo" src={logo} alt="Netflix Logo" />
+        </Link>
+        <nav className="header-nav-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/tv-shows" className="nav-link">
+            TV Shows
+          </Link>
+          <Link to="/movies" className="nav-link">
+            Movies
+          </Link>
+          <Link to="/latest" className="nav-link">
+            New & Popular
+          </Link>
+          <Link to="/my-list" className="nav-link">
+            My List
+          </Link>
+          <Link to="/browse-languages" className="nav-link">
+            Browse by Language
+          </Link>
+        </nav>
+      </div>
+      {/* RightSide Core Container Shell */}
+      <div className="header-right"></div>
+    </header>
+  );
+}
+export default Header;
