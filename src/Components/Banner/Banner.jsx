4ea05ce5e3d43a7033e7b0393b59d2052cc56932
Anan1729 <ananyawub20@gmail.com>
2026-08-26 19:38:00 -0700

    feat(T-10): complete safe movie data fetching and random selection logic

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./Banner.module.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Banner() {
   // Task T-10: Connected and working on Banner API Fetching
