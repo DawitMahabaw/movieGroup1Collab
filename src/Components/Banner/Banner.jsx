7775f450b8f8fa7a8eb15d4443b62db6fa790980
Anan1729 <ananyawub20@gmail.com>
2026-08-26 18:22:54 -0700

    initial commit to publish T-10 branch

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.module.css";
+
+function Banner() {
+  // Task T-10: Connected and working on Banner API Fetching
+
+  return (
+    <header className="banner">
+      <h1>Netflix Banner Placeholder</h1>
+    </header>
+  );
+}
+
+export default Banner;
